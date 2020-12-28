236c3ba27ba30674fdff3686b836869f757b07f0
Arthur Klassen <klassenorg@gmail.com>
2020-12-29 01:57:15 +0300

    fixed access log script

--- get_access_log_for_10min.sh
+++ get_access_log_for_10min.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 for i in {01..22};
-do ssh weblogic@atg-ps$i-prod 'sedtime=$(date --date "-10min" +"%d\/%b\/%Y:%H:%M:%S") && sed -n "/$sedtime/,$ p" /app/nginx/logs/atg-access.log' >> /app/jet/scripts/klassen/psaccesslog.txt &
+do ssh weblogic@atg-ps$i-prod 'sedtime=$(date --date "-10min" +"%d\/%b\/%Y:%H:%M:%S") && sed -n "/$sedtime/,$ p" /app/nginx/logs/atg-access.log' >> /app/jet/scripts/klassen/ps$i-accesslog.txt &
 done
 wait
+
+for i in {01..22};
+do cat /app/jet/scripts/klassen/ps$i-accesslog.txt >> psaccesslog.txt
+rm -rf /app/jet/scripts/klassen/ps$i-accesslog.txt
+done
