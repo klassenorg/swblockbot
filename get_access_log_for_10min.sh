8d69c8f5edf2374cd51b3cece8a75769647339fd
Arthur Klassen <klassenorg@gmail.com>
2020-12-29 11:53:22 +0300

    and some formatting changes

--- get_access_log_for_10min.sh
+++ get_access_log_for_10min.sh
@@ -6,5 +6,5 @@ done
 wait
 
 for i in {01..22};
-do echo ***PS$i-PROD*** >> /app/jet/scripts/klassen/psaccesslog.txt ; cat /app/jet/scripts/klassen/ps$i-accesslog.txt >> /app/jet/scripts/klassen/psaccesslog.txt ; rm -rf /app/jet/scripts/klassen/ps$i-accesslog.txt
+do printf "\n\n\n***PS$i-PROD***\n\n\n" >> /app/jet/scripts/klassen/psaccesslog.txt ; cat /app/jet/scripts/klassen/ps$i-accesslog.txt >> /app/jet/scripts/klassen/psaccesslog.txt ; rm -rf /app/jet/scripts/klassen/ps$i-accesslog.txt
 done
