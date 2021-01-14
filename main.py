389de99d440aaa98f8e55b56b67b4d0f893d0aae
Arthur Klassen <klassenorg@gmail.com>
2021-01-14 19:57:42 +0300

    disabled error handler b its broke bot

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import logging
 import creds
 import requests
 import json
-import traceback
-import html
 import datetime 
 #import time
 import re
@@ -115,30 +113,6 @@ logging.basicConfig(filename=creds.logdir,
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-
-def error_handler(update: Update, context: CallbackContext) -> None:
-    """Log the error and send a telegram message to notify the developer."""
-    # Log the error before we do anything else, so we can see it even if something breaks.
-    logger.error(msg="Exception while handling an update:", exc_info=context.error)
-
-    # traceback.format_exception returns the usual python message about an exception, but as a
-    # list of strings rather than a single string, so we have to join them together.
-    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
-    tb_string = ''.join(tb_list)
-
-    # Build the message with some markup and additional information about what happened.
-    # You might need to add some logic to deal with messages longer than the 4096 character limit.
-    message = (
-        f'An exception was raised while handling an update\n'
-        f'<pre>update = {html.escape(json.dumps(update.to_dict(), indent=2, ensure_ascii=False))}'
-        '</pre>\n\n'
-        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
-        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
-        f'<pre>{html.escape(tb_string)}</pre>'
-    )
-
-    # Finally, send the message
-    context.bot.send_message(chat_id=creds.L2_chat_id, text=message, parse_mode=ParseMode.HTML)
 
 check_active = True
 
@@ -821,8 +795,6 @@ def main():
     prepareDB()
     updater.job_queue.run_repeating(checkAndUnban, interval=300, first=0)
     updater.job_queue.run_repeating(find_bots, interval=7200, first=0)
-
-    #dp.add_error_handler(error_handler)
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("help", help))
